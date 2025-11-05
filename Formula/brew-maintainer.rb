bcf389570af6967faaed8cb412abefcdaae1118d
Luca Lorenzon <1104352+lucalorenzon@users.noreply.github.com>
2025-11-05 02:49:08 +0100

    fix:refactor formula to be more parametric respect github action

--- Formula/brew-maintainer.rb
+++ Formula/brew-maintainer.rb
@@ -1,51 +1,45 @@
 class BrewMaintainer < Formula
   desc "Automated Homebrew updater with logs, retry handling, and scheduled runs"
-  homepage "https://github.com/lucalorenzon/brew-maintainer"
-  url "https://github.com/lucalorenzon/brew-maintainer/archive/v0.1.2.tar.gz"
-  sha256 "99794ba12a16391781193f6ee18fe25f2c48fb3f7b1b2ff4b12143549c487a32"
+  homepage "https://github.com/<REPLACED_BY_GITHUB_ACTION>"
+  version "<REPLACED_BY_GITHUB_ACTION>"
+  url "<REPLACED_BY_GITHUB_ACTION>"
+  sha256 "<REPLACED_BY_GITHUB_ACTION>"
   license "MIT"
 
-  depends_on "rust" => :build
+  def install
+    bin.install "brew-maintainer"
+    (var/"log").mkpath
+  end
 
-  on_macos do
-    if Hardware::CPU.intel?
-      def install
-        system "cargo", "install", *std_cargo_args
-        (prefix/"brew-maintainer.plist").write plist
-      end
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>com.<REPLACED_BY_GITHUB_ACTION>.brew-maintainer</string>
 
-      def plist
-        <<~EOS
-          <?xml version="1.0" encoding="UTF-8"?>
-          <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-          <plist version="1.0">
-          <dict>
-            <key>Label</key>
-            <string>com.lucalorenzon.brew-maintainer</string>
-            <key>ProgramArguments</key>
-            <array>
-              <string>#{opt_bin}/brew-maintainer</string>
-            </array>
-            <key>StartInterval</key>
-            <integer>21600</integer>
-            <key>RunAtLoad</key>
-            <true/>
-            <key>StandardOutPath</key>
-            <string>/usr/local/var/log/brew-maintainer.log</string>
-            <key>StandardErrorPath</key>
-            <string>/usr/local/var/log/brew-maintainer-error.log</string>
-          </dict>
-          </plist>
-        EOS
-      end
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/brew-maintainer</string>
+        </array>
 
-      def post_install
-        system "mkdir", "-p", "/usr/local/var/log"
-        system "launchctl", "load", "-w", "#{prefix}/brew-maintainer.plist"
-      end
-    else
-      odie "Only x86_64 macOS is supported."
-    end
+        <!-- Run every 6 hours -->
+        <key>StartInterval</key>
+        <integer>21600</integer>
+
+        <key>RunAtLoad</key>
+        <true/>
+
+        <key>StandardOutPath</key>
+        <string>#{var}/log/brew-maintainer.log</string>
+
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/brew-maintainer-error.log</string>
+      </dict>
+      </plist>
+    EOS
   end
 
   test do
